36df0970c4f343168b6255ff9659755df64c073c
Natalia Leonteva <morein.mm@gmail.com>
2024-02-23 20:11:39 +0300

    Initial commit

--- app/src/main/java/ru/netology/nmedia/dto/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/dto/PostViewModel.kt
@@ -7,5 +7,5 @@ class PostViewModel: ViewModel() {
     val data = repository.getAll()
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
-
+ni
 }
