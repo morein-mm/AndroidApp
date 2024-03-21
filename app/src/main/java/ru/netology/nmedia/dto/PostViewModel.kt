127b312b809959a395b371613c4a0a7945e323eb
morein <morein.mm@gmail.com>
2024-03-21 08:37:37 +0300

    Initial commit

--- app/src/main/java/ru/netology/nmedia/dto/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/dto/PostViewModel.kt
@@ -3,6 +3,8 @@ package ru.netology.nmedia.dto
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.db.AppDb
+import ru.netology.nmedia.repository.PostRepositorySQLiteImpl
 
 val empty = Post(
     id = 0,
@@ -16,7 +18,12 @@ val empty = Post(
 )
 
 class PostViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository: PostRepository = PostRepositoryFileImpl(application)
+    //    private val repository: PostRepository = PostRepositoryFileImpl(application)
+    private val repository: PostRepository = PostRepositorySQLiteImpl(
+        AppDb.getInstance(application).postDao
+    )
+
+
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
